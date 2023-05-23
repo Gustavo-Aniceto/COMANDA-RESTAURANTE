95bbecce684d5a6227d4c2552d25a19e329144d0
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2023-05-23 10:18:19 -0300

    v3 Mais acrescimos (corrigir bugs)

--- COMANDARESTAURANTE.py
+++ COMANDARESTAURANTE.py
@@ -14,7 +14,7 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def add_item(item_list):
+def add_item(item_list, bebidas=None):
     while True:
         clear_screen()
         print("===========")
@@ -43,150 +43,253 @@ def add_item(item_list):
             continue
 
         if category == 1:
-            items = {
-                1: ("Moqueca", 38.0),
-                2: ("Feijoada", 39.90),
-                3: ("Baião de Dois", 95.0),
-                4: ("Arroz com Pequi", 25.90),
-                5: ("Virado à Paulista", 38.50),
-                6: ("Bobó de Camarão", 42.90),
-                7: ("Lasanha à Bolonhesa", 34.90),
-                8: ("Risoto de Funghi", 29.90),
-                9: ("macarronada", 24.99),
-                # Adicione mais pratos aqui
-            }
-        elif category == 2:
-            items = {
-                1: ("Coca Cola", 9.90),
-                2: ("Fanta (Laranja ou Uva)", 7.90),
-                3: ("Guarana", 6.90),
-                4: ("Pepsi", 7.90),
-                5: ("Itubaina", 5.90),
-                6: ("Suco de Laranja", 8.50),
-                7: ("Água Mineral", 3.50),
-                8: ("Chá Gelado", 4.90),
-                9: ("Cerveja", 14.90),
-                # Adicione mais bebidas aqui
-            }
+            pratosprotos = int(input("Pratos:\n1 - Frutos do mar\n2 - Massas\n3 - Carne\n4 - Peixes\nEscolha: "))
+
+            if pratosprotos == 1:
+                items = {
+                    1: ("Espaguete ao frutos do mar", 38.0),
+                    2: ("Camarão na champagne com arroz de maçã", 39.90),
+                    3: ("Paella", 95.0),
+                    4: ("Stroganov de frutos do mar", 25.90),
+                    5: ("Lula grelhada com arroz negro", 38.50),
+                    6: ("Lagosta ao thermidor", 42.90)
+                }
+            elif pratosprotos == 2:
+                items = {
+                    1: ("Capeletti de búfala e alcachofra", 38.0),
+                    2: ("Agnolotti de búfala e manjericão", 39.90),
+                    3: ("Nhoque de mandioquinha", 95.0),
+                    4: ("Macarronada", 25.90)
+                }
+            elif pratosprotos == 3:
+                items = {
+                    1: ("Filé mignon ao molho de ervas", 38.0),
+                    2: ("Picanha grelhada com batata recheada", 39.90),
+                    3: ("Paleta de cordeiro", 95.0),
+                    4: ("Picadinho à Marettimo", 25.90)
+                }
+            elif pratosprotos == 4:
+                items = {
+                    1: ("Filé de peixe fresco a belle Meuniere", 38.0),
+                    2: ("Badejo grelhado ao molho de alcaparras", 39.90),
+                    3: ("Badejo à brasileira", 95.0)
+                }
+            else:
+                clear_screen()
+                print("\n===================================")
+                print("CATEGORIA INVÁLIDA, TENTE NOVAMENTE!")
+                clear_screen()
+                input("Pressione Enter para continuar...")
+                continue
+
+            clear_screen()
+            print("PRATOS")
+            print("======")
+            for key, value in items.items():
+                print("{}. {} - R$ {:.2f}".format(key, value[0], value[1]))
+            print("0 - Voltar")
+            print("--------------")
+            prato = int(input("Escolha o prato: "))
+
+            if prato == 0:
+                continue
+
+            if prato < 1 or prato > len(items):
+                clear_screen()
+                print("\n===================================")
+                print("PRATO INVÁLIDO, TENTE NOVAMENTE!")
+                print("===================================\n")
+                input("Pressione Enter para continuar...")
+                continue
+
+            item = items[prato]
+            item_list.append(Produto(item[1], item[0]))
+
+            if category == 2:
+                bebidas = int(input("Refrigerantes:\n1 - Sucos Naturais\n2 - Agua\n3 - Cervejas\n4 -\nEscolha: "))
+
+                if bebidas == 1:
+                    items = {
+                        1: ("Coca Cola (2L)", 38.0),
+                        2: ("Fanta Laranja (2L)", 39.90),
+                        3: ("Fanta uva (2L)", 95.0),
+                        4: ("Guarana Antartica (2L)", 25.90),
+                        5: ("pepsi (2L)", 38.50),
+                        6: ("Itubaina (2L)", 42.90)
+                    }
+                elif bebidas == 2:
+                    items = {
+                        1: ("Laranja (1L)", 8.0),
+                        2: ("Abacaxi c/ Hortelã (1L)", 9.90),
+                        3: ("Cupuacu (1L)", 5.0),
+                        4: ("Lemonede (1L)", 5.90)
+                    }
+                elif bebidas == 3:
+                    items = {
+                        1: ("Agua Mineral (700ml)", 8.0),
+                        2: ("Agua c/ Gas (700ml)", 9.90),
+                        3: ("Agua Tonica (400ml)", 5.0),
+                    }
+                elif bebidas == 4:
+                    items = {
+                        1: ("Heineken", 38.0),
+                        2: ("Corona", 39.90),
+                        3: ("Cerveja Artesanal", 95.0)
+                    }
+                else:
+                    clear_screen()
+                    print("\n===================================")
+                    print("CATEGORIA INVÁLIDA, TENTE NOVAMENTE!")
+                    clear_screen()
+                    input("Pressione Enter para continuar...")
+                    continue
+
+                clear_screen()
+                print("BEBIDAS")
+                print("======")
+                for key, value in items.items():
+                    print("{}. {} - R$ {:.2f}".format(key, value[0], value[1]))
+                print("0 - Voltar")
+                print("--------------")
+                bebidas = int(input("Escolha a Bebida: "))
+
+                if bebidas == 0:
+                    continue
+
+                if bebidas < 1 or bebidas > len(items):
+                    clear_screen()
+                    print("\n===================================")
+                    print("BEBIDA INVÁLIDO, TENTE NOVAMENTE!")
+                    print("===================================\n")
+                    input("Pressione Enter para continuar...")
+                    continue
+
+                item = items[bebidas]
+                item_list.append(Produto(item[1], item[0]))
+
+
+
         elif category == 3:
             items = {
-                1: ("Feijão", 9.90),
-                2: ("Farofa", 3.90),
-                3: ("Batata Frita", 10.90),
-                4: ("Pure", 7.90),
-                5: ("Salada", 6.90),
-                6: ("Vinagrete", 4.50),
-                7: ("Mandioca Frita", 8.90),
-                8: ("Couve Refogada", 6.50),
-                9: ("Torresmo", 9.90),
-
-                # Adicione mais acompanhamentos aqui
+                1: ("Batata frita", 12.0),
+                2: ("Salada mista", 15.0),
+                3: ("Arroz branco", 10.0),
+                4: ("Legumes grelhados", 15.0)
             }
+
+            clear_screen()
+            print("ACOMPANHAMENTOS")
+            print("===============")
+            for key, value in items.items():
+                print("{}. {} - R$ {:.2f}".format(key, value[0], value[1]))
+            print("0 - Voltar")
+            print("--------------")
+            acompanhamento = int(input("Escolha o acompanhamento: "))
+
+            if acompanhamento == 0:
+                continue
+
+            if acompanhamento < 1 or acompanhamento > len(items):
+                clear_screen()
+                print("\n===================================")
+                print("ACOMPANHAMENTO INVÁLIDO, TENTE NOVAMENTE!")
+                print("===================================\n")
+                input("Pressione Enter para continuar...")
+                continue
+
+            item = items[acompanhamento]
+            item_list.append(Produto(item[1], item[0]))
 
         elif category == 4:
             items = {
-                1: ("Pudim", 8.50),
-                2: ("Sorvete", 6.90),
-                3: ("Torta de Limão", 12.0),
-                4: ("Brigadeiro", 4.50),
-                5: ("Mousse de Chocolate", 9.90),
-                6: ("Cheesecake de Morango", 16.90),
-                7: ("Quindim", 7.90),
-                8: ("Tiramisu", 18.90),
-                9: ("Açai", 15.90),
-                # Adicione mais sobremesas aqui
+                1: ("Pudim de leite", 8.0),
+                2: ("Mousse de chocolate", 10.0),
+                3: ("Torta de limão", 12.0),
+                4: ("Sorvete de creme", 6.0)
             }
-        else:
+
             clear_screen()
-            print("\n===================================")
-            print("CATEGORIA INVÁLIDA, TENTE NOVAMENTE!")
-            print("===================================\n")
-            input("Pressione Enter para continuar...")
-            continue
+            print("SOBREMESAS")
+            print("==========")
+            for key, value in items.items():
+                print("{}. {} - R$ {:.2f}".format(key, value[0], value[1]))
+            print("0 - Voltar")
+            print("--------------")
+            sobremesa = int(input("Escolha a sobremesa: "))
+
+            if sobremesa == 0:
+                continue
+
+            if sobremesa < 1 or sobremesa > len(items):
+                clear_screen()
+                print("\n===================================")
+                print("SOBREMESA INVÁLIDA, TENTE NOVAMENTE!")
+                print("===================================\n")
+                input("Pressione Enter para continuar...")
+                continue
+
+            item = items[sobremesa]
+            item_list.append(Produto(item[1], item[0]))
 
         clear_screen()
-        print("===========")
-        print("COMANDA | {}º Item".format(len(item_list) + 1))
-        print("===========")
+        print("\n===================================")
+        print("PRODUTO ADICIONADO À COMANDA")
+        print("===================================\n")
+        input("Pressione Enter para continuar...")
+        clear_screen()
+
+
+    while True:
+        clear_screen()
+        print("============")
+        print("COMANDA")
+        print("============")
         print()
-        print("CARDAPIO")
-        print("========")
-        print()
-        for index, item in items.items():
-            print(f"{index} - {item[0]} - R$ {item[1]}")
-        print()
+        print("1 - Adicionar item")
+        print("2 - Exibir comanda")
         print("0 - Encerrar")
         print("--------------")
-        num = int(input("Opção: "))
+        opcao = int(input("Opção: "))
 
-        if num == 0:
+        if opcao == 0:
             break
 
-        if num < 1 or num > len(items):
+        if opcao < 1 or opcao > 2:
             clear_screen()
             print("\n===================================")
-            print("VALOR INCORRETO, TENTE NOVAMENTE!")
+            print("OPÇÃO INVÁLIDA, TENTE NOVAMENTE!")
             print("===================================\n")
             input("Pressione Enter para continuar...")
             continue
 
-        name, preco = items[num]
-        qntd = int(input("\nQuantidade: "))
+        if opcao == 1:
+            add_item(item_list, bebidas)
 
-        item = Produto(preco, name)
-        item.quantidade = qntd
-        item_list.append(item)
+        elif opcao == 2:
+            clear_screen()
+            print("COMANDA")
+            print("=======")
+            total = 0.0
 
+            for i, item in enumerate(item_list):
+                print("{} - {} - R$ {:.2f}".format(i + 1, item.nome, item.valor))
+                total += item.valor
 
-def remove_item(item_list):
-    clear_screen()
-    print("============")
-    print("REMOVER ITEM")
-    print("============\n")
-
-    if not item_list:
-        print("A comanda está vazia. Não há itens para remover.")
-        input("\nPressione Enter para continuar...")
-        return
-
-    print("COMANDA")
-    print("-------")
-    for i, item in enumerate(item_list):
-        print(f"{i+1} - {item.nome} - R$ {item.valor} - Quantidade: {item.quantidade}")
-
-    print()
-    item_index = int(input("Digite o número do item que deseja remover: "))
-
-    if item_index < 1 or item_index > len(item_list):
-        print("Índice inválido. Tente novamente.")
-        input("\nPressione Enter para continuar...")
-    else:
-        item_list.pop(item_index - 1)
-        print("Item removido com sucesso.")
-        input("\nPressione Enter para continuar...")
-
-
-def show_order(item_list):
-    total = 0
-
-    clear_screen()
-    print("\t\t\t============")
-    print("\t\t\tDESCRITIVO")
-    print("\t\t\t============\n")
-
-    print("----------------------------------------------------------------")
-    print("Nome\t\t|   Quantidade\t|  Valor Unit\t| Sub-Total")
-    print("----------------------------------------------------------------")
-    for item in item_list:
-        sub_total = item.quantidade * item.valor
-        print("{:<12}|{:>8d}\t\t| R$ {:>8.2f}\t| R$ {:>8.2f}".format(item.nome, item.quantidade, item.valor, sub_total))
-        total += sub_total
-    print("----------------------------------------------------------------")
-    print("\nTotal de compras: R$ {:.2f}".format(total))
-
+            print("--------------")
+            print("TOTAL: R$ {:.2f}".format(total))
+            print("\n===================================\n")
+            input("Pressione Enter para continuar...")
     now = time.localtime()
     date = "{}/{}/{}".format(now.tm_mday, now.tm_mon, now.tm_year)
     print("Data: {}".format(date))
+
+
+def show_order(item_list):
+    pass
+
+
+def remove_item(item_list):
+    pass
 
 
 def make_payment(item_list):
@@ -228,3 +331,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
